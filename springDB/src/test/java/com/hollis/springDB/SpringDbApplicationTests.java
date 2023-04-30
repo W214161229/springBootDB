b1a27cd539e6fa947329ea79cacc6a0aa067fd97
W214161229 <128067814+W214161229@users.noreply.github.com>
2023-04-30 02:06:28 -0500

    Add files via upload

---
+++ springDB/src/test/java/com/hollis/springDB/SpringDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hollis.springDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
